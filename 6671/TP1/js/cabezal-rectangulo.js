18e3cf67a044b6e5042d06eb035e6d5d5559784f
Sebastian D'Alessandro Szymanowski <dalessandrosj@gmail.com>
2021-07-30 22:13:47 -0300

    Usar extrusor para el cuboide

---
+++ 6671/TP1/js/cabezal-rectangulo.js
@@ -0,0 +1,34 @@
+class CabezalRectangulo extends Cabezal {
+    constructor(ancho, largo) {
+        super();
+        const mitadA = ancho/2;
+        const mitadB = largo/2;
+        this.vertices = [[-mitadA, -mitadB],
+                         [-mitadA, mitadB],
+                         [mitadA, mitadB],
+                         [mitadA, -mitadB]];
+        this.normales = [[-1, 0, 0],
+                         [0, 0, 1],
+                         [1, 0, 0],
+                         [0, 0, -1]];
+
+        let v = [this.vertices[0]];
+        for (let i = 1; i < this.vertices.length; i++) {
+            v.push(this.vertices[i]);
+            v.push(this.vertices[i]);
+        }
+        v.push(this.vertices[0]);
+        this.vertices = v;
+
+        let n = [];
+        for (let i = 0; i < this.normales.length; i++) {
+            n.push(this.normales[i]);
+            n.push(this.normales[i]);
+        }
+        this.normales = n;
+    }
+
+    getCentro() {
+        return [0, 0, 0];
+    }
+}
