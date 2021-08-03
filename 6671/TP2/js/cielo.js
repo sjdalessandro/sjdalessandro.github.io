c2cdb74d470674a1693687c819e1a7941a9cfde6
Sebastian D'Alessandro Szymanowski <dalessandrosj@gmail.com>
2021-08-03 02:55:04 -0300

    Esperar la carga de las imagenes del cubemap

--- 6671/TP2/js/cielo.js
+++ 6671/TP2/js/cielo.js
@@ -2,62 +2,11 @@ class Cielo {
     constructor() {
         this.modificada = false;
 
-        // Create a texture.
-        var texture = gl.createTexture();
-        gl.bindTexture(gl.TEXTURE_CUBE_MAP, texture);
-
-        const faceInfos = [
-            {
-            target: gl.TEXTURE_CUBE_MAP_POSITIVE_X,
-            url: './texturas/Cielo/px.png',
-            },
-            {
-            target: gl.TEXTURE_CUBE_MAP_NEGATIVE_X,
-            url: './texturas/Cielo/nx.png',
-            },
-            {
-            target: gl.TEXTURE_CUBE_MAP_POSITIVE_Y,
-            url: './texturas/Cielo/ny.png',
-            },
-            {
-            target: gl.TEXTURE_CUBE_MAP_NEGATIVE_Y,
-            url: './texturas/Cielo/py.png',
-            },
-            {
-            target: gl.TEXTURE_CUBE_MAP_POSITIVE_Z,
-            url: './texturas/Cielo/pz.png',
-            },
-            {
-            target: gl.TEXTURE_CUBE_MAP_NEGATIVE_Z,
-            url: './texturas/Cielo/nz.png',
-            },
-        ];
-
-        faceInfos.forEach(async (faceInfo) => {
-            this.load(faceInfo, texture);
-        });
+        gl.bindTexture(gl.TEXTURE_CUBE_MAP, texturas.cielo);
         gl.generateMipmap(gl.TEXTURE_CUBE_MAP);
         gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
 
         this.generar();
-    }
-
-    async load(faceInfo, texture) {
-        const {target, url} = faceInfo;
-
-        // setup each face so it's immediately renderable
-        gl.texImage2D(target, 0, gl.RGBA, 512, 512, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
-
-        // Asynchronously load an image
-        const image = new Image();
-        image.src = url;
-
-        image.addEventListener('load', function() {
-        // Now that the image has loaded make copy it to the texture.
-        gl.bindTexture(gl.TEXTURE_CUBE_MAP, texture);
-        gl.texImage2D(target, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-        gl.generateMipmap(gl.TEXTURE_CUBE_MAP);
-        });
     }
 
     generar() {
