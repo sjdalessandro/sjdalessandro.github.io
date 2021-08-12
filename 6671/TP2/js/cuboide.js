c8b464d4596938c56927f624c1c11fa3f12d9a85
Sebastian D'Alessandro Szymanowski <dalessandrosj@gmail.com>
2021-08-12 00:28:44 -0300

    Modificar apariencia del suelo

--- 6671/TP2/js/cuboide.js
+++ 6671/TP2/js/cuboide.js
@@ -1,6 +1,6 @@
 class Cuboide extends Extrusor {
     constructor(ladoA, ladoB, ladoC, tapar, tipoTextura, tipoTexturaEnTapa) {
-        let cabezalRectangulo = new CabezalRectangulo(ladoA, ladoB, ladoA/5);
+        let cabezalRectangulo = new CabezalRectangulo(ladoA, ladoB);
         let trayectoriaBase = new TrayectoriaRecta(ladoC);
         super(cabezalRectangulo, trayectoriaBase, tapar, tipoTextura, tipoTexturaEnTapa);
     }
